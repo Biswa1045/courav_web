e7a5944a7da0cd6efe542df9d70f18e03252f315
biswa1045 <biswajitsahu1045@gmail.com>
2025-03-05 14:47:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyle } from "./style";
-import Navbar from "./components/Nav/Navbar";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 import PageNotFound from "./pages/notFound";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router>
-        
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
